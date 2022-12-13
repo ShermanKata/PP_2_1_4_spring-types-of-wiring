816b32ede6cf0db5bd02dbdb5f9d518caff456b0
ShermanKata <pvtlab@yandex.ru>
2022-12-13 17:11:43 +0500

    Commit

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -4,9 +4,6 @@ import koschei.config.AppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
